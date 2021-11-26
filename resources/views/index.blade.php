aa2b02547d2b19daa44873711e6c333228c16b09
RyndychRD <r.roman1234@yandex.ru>
2021-11-26 10:29:46 +0300

    finish

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Title</title>
+    <title>Netwrix Partner Locator</title>
     <link href="{{mix('css/app.css')}}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.5.1/chosen.min.css">
+{{--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.5.1/chosen.min.css">--}}
 
 
 </head>
